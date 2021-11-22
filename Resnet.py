88add751980cda30c9775a35f049dc1334725f76
Fenther586 <57915662+Fenther586@users.noreply.github.com>
2021-11-22 15:05:10 +0000

    added comments

--- Resnet.py
+++ Resnet.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
-from torch.utils.data import Dataset, DataLoader, dataloader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split
 
 from PIL import Image
 from tqdm import tqdm
@@ -12,39 +12,37 @@ import copy
 import math
 import csv
 
-# IMPORTS MOVED TO BOTTOM OF FILE TO AVOID CIRCULAR REFERENCING
 from Globals import labels
 
-### https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
-### https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel
-### https://pytorch.org/hub/pytorch_vision_resnet/
-### https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
-### https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
-
-
+#convert labels to numerical categories
 label_map = {'angry':0, 'disgust':1, 'fear':2, 'happy':3, 'neutral':4, 'sad':5, 'surprise':6}
+# move all data and model to GPU if available
 my_device = "cuda" if torch.cuda.is_available() else "cpu"
 
+# set the requires grad parameter on model to false for all layers
 def freeze_layers(model):
     for layer in model.parameters():
         layer.requires_grad = False
 
+# modify the final layer or add an extra hidden layer before that of a given size
 def add_new_layers(model, extra_nodes=0):
     if extra_nodes != 0:
-        model.fc = torch.nn.Linear(512, extra_nodes)
+        model.fc = torch.nn.Linear(512, extra_nodes) # must take 512 inputs
         model = nn.Sequential(
             model,
             torch.nn.Linear(extra_nodes, len(labels))
         )
     else:
-        model.fc = torch.nn.Linear(512, len(labels))
+        model.fc = torch.nn.Linear(512, len(labels)) # must take 512 inputs from previous layer
 
     return model
 
+# download (or access cahced) model, will get pretrained versino if unspecified
 def get_resnet_model(pretrained=True):
     model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=pretrained) ## is a 1000 output net as it was trained as 1000 image classifier
     return model
 
+# load all the images into a single list
 def flat_load_images(dir="train/"):
     #Primary image container
     my_images = []
@@ -60,10 +58,10 @@ def flat_load_images(dir="train/"):
     #Return the images and corresponding labels
     return my_images, my_labels
 
-
+# custom dataset class to work by loading all images into memory to be used, only works as images are small so can be easily held in memory
 class CustomDataset(Dataset):
     def __init__(self, dir, transform=None):
-        images, labels = flat_load_images(dir)
+        images, labels = flat_load_images(dir) # load all images
         self.transform = transform
         self.img_labels = labels
         self.my_images = images
@@ -73,7 +71,7 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         if self.transform:
-            image = self.transform(self.my_images[idx])
+            image = self.transform(self.my_images[idx]) # apply given transform if it exists
         else:
             image = self.my_images[idx]
 
@@ -81,30 +79,31 @@ class CustomDataset(Dataset):
 
         return image, label_map[label] 
         
-
+# get the validation and training dataloaders that will have split training dataset by a given ratio
 def get_train_dataloaders(validation_ratio=0.2):
 
     preprocess = T.Compose([
         T.Lambda(lambda x : x.convert("RGB")),
         T.Resize(256),
         T.CenterCrop(224),
-        T.RandomHorizontalFlip(p=0.5),
-        T.RandomVerticalFlip(p=0.5),
-        T.RandomApply(transforms=[T.RandomRotation(degrees=(0,360))], p=0.5),
-        T.RandomPerspective(distortion_scale=0.5, p=0.5),
+        T.RandomHorizontalFlip(p=0.5), # randomly flip
+        T.RandomVerticalFlip(p=0.5), # randomly flip
+        T.RandomApply(transforms=[T.RandomRotation(degrees=(0,360))], p=0.5), # randomly apply random rotation
+        T.RandomPerspective(distortion_scale=0.5, p=0.5), # randomly apply a random perspective
         T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # normalise tensor for resnet as specified
     ])
 
     train_dataset = CustomDataset("train/", transform=preprocess)
+    #split train and validation dataset randomly
     train_dataset, validation_dataset = random_split(train_dataset, [math.ceil(len(train_dataset) * (1-validation_ratio)), math.floor(len(train_dataset) * validation_ratio)])
 
     train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=64, shuffle=False) # no need to shuffle validation data either
+    validation_dataloader = DataLoader(validation_dataset, batch_size=64, shuffle=False) # no need to shuffle validation data
 
     return train_dataloader, validation_dataloader
 
-
+# train the model
 def train_model(model, t_dataloader, v_dataloader, learn_rate=1e-3, min_val_loss_improvement=0.1, improvement_buffer=5, absolute_max_epochs=25):
     model.train() # set model to training mode
     model.to(my_device) # send model to device
@@ -115,33 +114,36 @@ def train_model(model, t_dataloader, v_dataloader, learn_rate=1e-3, min_val_loss
             learning_layers.append(layer)
             # print("Training layer: ", name)
     
+    # specify loss and optimiser functions
     loss_func = nn.CrossEntropyLoss()
     optimizer_func = torch.optim.SGD(learning_layers, learn_rate)
 
-    best_loss = 1e10
-    best_weights = copy.deepcopy(model.state_dict())
+    best_loss = 1e10 # set initial loss
+    best_weights = copy.deepcopy(model.state_dict()) # get initial best weights which are whatever they have been initialised to
 
     loss_thresh_epoch = 0
     running_loss_thresh = 1e10
 
     for epoch in range(absolute_max_epochs):
-        model.train()
+        model.train() # set model to train mode
         running_training_loss = 0
-        for (X, y) in tqdm(t_dataloader):
+        for (X, y) in tqdm(t_dataloader): # for each batch in the dataloader
             X, y = X.to(my_device), y.to(my_device)
 
+            # make preditiction and calculate loss
             prediction = model(X)
             loss = loss_func(prediction, y)
             running_training_loss += loss
 
+            # update model weights based off of prediction
             optimizer_func.zero_grad()
             loss.backward()
             optimizer_func.step()
 
-        model.eval()
+        model.eval() # set model to eval mode
         num_correct = 0
         running_val_loss = 0
-        for (X, y) in v_dataloader:
+        for (X, y) in v_dataloader: # for each batch in validation dataset
             X, y = X.to(my_device), y.to(my_device)
 
             prediction = model(X)
@@ -153,22 +155,27 @@ def train_model(model, t_dataloader, v_dataloader, learn_rate=1e-3, min_val_loss
             batch_correct = (predicted_labels == y).sum()
             num_correct += batch_correct
 
+        # calculate metrics
         epoch_val_loss = running_val_loss / len(v_dataloader)
         epoch_val_acc = num_correct / len(v_dataloader.dataset)
 
         print(f"Epoch {epoch}: Training Loss: {running_training_loss / len(t_dataloader)}, Validation Loss: {epoch_val_loss}, Validation Accuracy: {epoch_val_acc}")
 
+        # if there is a new best loss, i.e. best model
         if epoch_val_loss < best_loss:
             best_loss = epoch_val_loss
             best_weights = copy.deepcopy(model.state_dict())
             print("new best loss")
-        
+
+        # if the loss has improved by the minimum threshold
         if running_loss_thresh - epoch_val_loss > min_val_loss_improvement:
             print("Improved by threshold so resetting epoch count")
             running_loss_thresh = epoch_val_loss
             loss_thresh_epoch = epoch
         else:
             print(f"current epoch count {epoch - loss_thresh_epoch}")
+
+        # if the loss has not improved by the buffer amount in the given number of epochs, early stop training
         if epoch - loss_thresh_epoch >= improvement_buffer:
             print(f"Early stopping as no significant improvement in {improvement_buffer} epochs")
             break
@@ -176,12 +183,14 @@ def train_model(model, t_dataloader, v_dataloader, learn_rate=1e-3, min_val_loss
         
     # torch.save(best_weights, f"model_loss_{best_loss}.pth")
 
-    return best_weights, best_loss
+    return best_weights, best_loss # return weights and loss
 
+
+# perform hyperparameter grid search
 def hyper_parameter_search(model, train_dataloader, validation_dataloader):
 
-    extra_layer_nodes = [0, 7, 16, 32, 64, 128]
-    learning_rates = [0.0001, 0.001, 0.01, 0.1]
+    extra_layer_nodes = [0, 7, 16, 32, 64, 128] # search different model shapes
+    learning_rates = [0.0001, 0.001, 0.01, 0.1] # search different learning rates
 
     best_model = {"best_loss":1e10}
 
@@ -195,7 +204,7 @@ def hyper_parameter_search(model, train_dataloader, validation_dataloader):
 
             w, l = train_model(model_copy, train_dataloader, validation_dataloader, learn_rate, min_val_loss_improvement=0.03)
 
-            if (l < best_model["best_loss"]):
+            if (l < best_model["best_loss"]): # if new best model save its details to dictionary
                 best_model["best_loss"] = l
                 best_model["best_weights"] = w
                 best_model["layer_nodes"] = ex_layer_nodes
@@ -204,6 +213,7 @@ def hyper_parameter_search(model, train_dataloader, validation_dataloader):
     with open(f"model-{best_model['best_loss']:.4f}.pkl", "wb") as f: ## remember to load with "rb" flag
         pickle.dump(best_model, f, pickle.HIGHEST_PROTOCOL)
 
+# get the model and train the last few layers of the resnet model
 def resnet_train():
     model = get_resnet_model()
     freeze_layers(model)
@@ -213,10 +223,10 @@ def resnet_train():
     hyper_parameter_search(model, train_dataloader, validation_dataloader)
 
 
-
+# get the dataloader for the test dataset
 def get_test_dataloader(path):
 
-    preprocess = T.Compose([
+    preprocess = T.Compose([ # shouldn't apply any data augmentation transforms, only to get in right shape for tensor
         T.Lambda(lambda x : x.convert("RGB")),
         T.Resize(256),
         T.CenterCrop(224),
@@ -230,15 +240,15 @@ def get_test_dataloader(path):
     return test_dataloader
 
 
-
+# iterate over test dataset
 def resnet_test(path):
     with open(path, "rb") as f:
         model_dict = pickle.load(f)
 
     model = get_resnet_model(pretrained=False) # get blank resnet model
-    model = add_new_layers(model, model_dict["layer_nodes"])
+    model = add_new_layers(model, model_dict["layer_nodes"]) # make model into correct shape
 
-    model.load_state_dict(model_dict["best_weights"])
+    model.load_state_dict(model_dict["best_weights"]) # apply best model weights
 
     model.eval()
 
@@ -265,6 +275,7 @@ def resnet_test(path):
             p_amounts.append(p_amount)
             accs.append(accuracy.item())
         
+        # write calculated accuracy to csv file
         with open(f"Resnet-{perturb_type}.csv", 'w', newline="") as res_f:
             writer = csv.writer(res_f)
             writer.writerow(p_amounts)
